1385dd1dee0c6cb19a9e0c4a140c34d662e7bb64
Subhram Subhrajyoti Rana <Subhram@MacBookPro.local>
2024-10-06 16:47:02 +0530

    tested the test set

--- ANN/parabolic_surface_ann.py
+++ ANN/parabolic_surface_ann.py
@@ -32,9 +32,11 @@ def main():
     # ANN.add(Dense(1, input_dim=2, activation='linear', name='output_layer'))  # linear model
 
     # ----- nonlinear feed-forward ANN model -----
-    ANN.add(Dense(30, input_dim=2, activation='relu', name='hidden_1'))
-    ANN.add(Dense(30, activation='relu', name='hidden_2'))
-    ANN.add(Dense(30, activation='relu', name='hidden_3'))
+    ANN.add(Dense(50, input_dim=2, activation='relu', name='hidden_1'))
+    ANN.add(Dense(50, activation='relu', name='hidden_2'))
+    ANN.add(Dense(50, activation='relu', name='hidden_3'))
+    ANN.add(Dense(50, activation='relu', name='hidden_4'))
+    ANN.add(Dense(50, activation='relu', name='hidden_5'))
     ANN.add(Dense(1, activation='linear', name='output_layer'))
 
     ANN.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae'])
@@ -42,7 +44,14 @@ def main():
 
     ANN.fit(normalised_input_train, output_train, validation_split=0.2, epochs=100)
 
-    # -------------- predictions --------------- #
+    # ---- post training evaluation --
+    normalised_input_test = input_scaler.transform(input_test)
+    mean_squared_error, mean_absolute_error = ANN.evaluate(normalised_input_test, output_test)
+    print(f'mean_squared_error: {mean_squared_error}')
+    print(f'mean_absolute_error: {mean_absolute_error}')
+    # --------------------------------
+
+    # -------------- example predictions --------------- #
     normalised_input_test = input_scaler.transform(input_test)
     print(f'predicted value: \n{ANN.predict(normalised_input_test[10:15])}')
     print(f'real outputs: \n{output_test[10:15]}')
